8e6b628b49c125a108fbfaa075aa6fc3db4cf06b
ManuelBasso <manuel.basso.89@gmail.com>
2023-10-12 16:53:32 +0200

    exercise not completed

--- 24Array.java
+++ 24Array.java
@@ -1,1 +1,16 @@
 
+public class Main {
+    public static void main(String args[]) {
+    int newArray[]={1,2,3,4,5,1};
+        delete(newArray);
+    }
+
+ public static void delete(int a[]){
+
+
+
+
+                }
+            }
+        }
+
