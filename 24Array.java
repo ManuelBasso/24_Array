fbd60ec63c8852bf604e977e3dc01e7c8098e858
ManuelBasso <manuel.basso.89@gmail.com>
2023-10-16 09:10:37 +0200

    exercise completed

--- 24Array.java
+++ 24Array.java
@@ -1,16 +1,26 @@
+//Scrivere un programma che contenga un metodo che permetta di rimuovere gli elementi duplicati
+//in un array di interi. L'array ottenuto dovrà essere stampato a video.
+
+//Pair programming
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
-    int newArray[]={1,2,3,4,5,1};
+    int newArray[]={6,1,1,2,3,4,6,5,2,10,11};
         delete(newArray);
     }
 
  public static void delete(int a[]){
-
-
-
-
-                }
-            }
+     
+    
+    for(int i = 0; i < a.length; i++){
+        Arrays.sort(a);
+        if ((i == a.length-1)|| (a[i]!=a[i+1])) {
+            System.out.print(a[i]+ " ");
         }
+    }
+}
+}
+
 
